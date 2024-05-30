85cc77828e34c0fa4c266a6a34d1b061ba88624b
akbararif1103 <123220004@student.upnyk.ac.id>
2024-05-30 13:31:25 +0700

    Control Handling & Kursi sama nambahin db

--- database/projek_akhir_pbo.sql
+++ database/projek_akhir_pbo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2024 at 11:06 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Waktu pembuatan: 30 Bulan Mei 2024 pada 08.30
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `jadwal`
+-- Struktur dari tabel `jadwal`
 --
 
 CREATE TABLE `jadwal` (
@@ -35,43 +35,46 @@ CREATE TABLE `jadwal` (
   `harga` varchar(20) NOT NULL,
   `kodePesawat` varchar(10) NOT NULL,
   `kodeKotaAwal` varchar(10) NOT NULL,
-  `kodeKotaTujuan` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `kodeKotaTujuan` varchar(10) NOT NULL,
+  `kursiDiambil` int(11) DEFAULT 0,
+  `kursiTersedia` int(11) DEFAULT 0,
+  `statusKuota` varchar(10) GENERATED ALWAYS AS (case when `kursiDiambil` = `kursiTersedia` then 'penuh' else 'tersedia' end) STORED
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `jadwal`
+-- Dumping data untuk tabel `jadwal`
 --
 
-INSERT INTO `jadwal` (`id_jadwal`, `kodeJadwal`, `jamKeberangkatan`, `jamKedatangan`, `harga`, `kodePesawat`, `kodeKotaAwal`, `kodeKotaTujuan`) VALUES
-(2, 'JD01', '10:00:00', '12:00:00', '50000', 'PW01', 'KT01', 'KT02'),
-(4, 'JD03', '09:00', '12:00', '1', 'PW01', 'KT03', 'KT02'),
-(8, 'JD04', '20:00', '21:00', '200', 'PW02', 'KT01', 'KT03');
+INSERT INTO `jadwal` (`id_jadwal`, `kodeJadwal`, `jamKeberangkatan`, `jamKedatangan`, `harga`, `kodePesawat`, `kodeKotaAwal`, `kodeKotaTujuan`, `kursiDiambil`, `kursiTersedia`) VALUES
+(12, 'JD01', '10:00', '12:00', '1200000', 'PW01', 'KT01', 'KT03', 110, 100),
+(13, 'JD02', '10:00', '12:00', '200000', 'PW03', 'KT01', 'KT03', 100, 100),
+(14, 'JD03', '08:00', '10:00', '190822', 'PW01', 'KT01', 'KT03', 1, 900);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kota`
+-- Struktur dari tabel `kota`
 --
 
 CREATE TABLE `kota` (
   `id_kota` int(11) NOT NULL,
   `kodeKota` varchar(10) NOT NULL,
   `kota` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `kota`
+-- Dumping data untuk tabel `kota`
 --
 
 INSERT INTO `kota` (`id_kota`, `kodeKota`, `kota`) VALUES
 (1, 'KT01', 'Jogja'),
-(2, 'KT02', 'Jakarta'),
-(3, 'KT03', 'Semarang');
+(2, 'KT02', 'Semarang'),
+(3, 'KT03', 'Jakarta');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pemesanan`
+-- Struktur dari tabel `pemesanan`
 --
 
 CREATE TABLE `pemesanan` (
@@ -81,32 +84,33 @@ CREATE TABLE `pemesanan` (
   `noHp` varchar(20) NOT NULL,
   `kota_awal` int(11) NOT NULL,
   `kota_tujuan` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pesawat`
+-- Struktur dari tabel `pesawat`
 --
 
 CREATE TABLE `pesawat` (
   `id_pesawat` int(11) NOT NULL,
   `kodePesawat` varchar(10) NOT NULL,
   `pesawat` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pesawat`
+-- Dumping data untuk tabel `pesawat`
 --
 
 INSERT INTO `pesawat` (`id_pesawat`, `kodePesawat`, `pesawat`) VALUES
 (1, 'PW01', 'Garuda Indonesia'),
-(2, 'PW02', 'Terbang jaya');
+(2, 'PW02', 'Terbang jaya'),
+(6, 'PW03', 'Batik');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -115,10 +119,10 @@ CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`) VALUES
@@ -126,30 +130,34 @@ INSERT INTO `users` (`id_user`, `nama`, `email`, `username`, `password`) VALUES
 (2, 'wijdan', 'wijdan@gmail.com', '321', '321'),
 (4, 'kuro', 'kuro', '1234', '1234'),
 (5, 'shiro', 'shirio@gmail.com', '12345', '12345'),
-(12, '123', '123', '098', '098');
+(12, '123', '123', '098', '098'),
+(13, 'Akbar', '1', 'arif1', '123'),
+(18, 'a', 'a', 'rolly', '123'),
+(19, 'Ageng', 'ageng@gmail.com', 'ageng', '123');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `jadwal`
+-- Indeks untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
   ADD PRIMARY KEY (`id_jadwal`),
+  ADD UNIQUE KEY `kodeJadwal` (`kodeJadwal`),
   ADD KEY `pesawatJadwal` (`kodePesawat`),
   ADD KEY `kotaAwalJadwal` (`kodeKotaAwal`),
   ADD KEY `kotaTujuanJadwal` (`kodeKotaTujuan`);
 
 --
--- Indexes for table `kota`
+-- Indeks untuk tabel `kota`
 --
 ALTER TABLE `kota`
   ADD PRIMARY KEY (`id_kota`),
   ADD UNIQUE KEY `kodeKota` (`kodeKota`);
 
 --
--- Indexes for table `pemesanan`
+-- Indeks untuk tabel `pemesanan`
 --
 ALTER TABLE `pemesanan`
   ADD PRIMARY KEY (`id_pesan`),
@@ -157,59 +165,59 @@ ALTER TABLE `pemesanan`
   ADD KEY `kota_tujuan_pesan` (`kota_tujuan`);
 
 --
--- Indexes for table `pesawat`
+-- Indeks untuk tabel `pesawat`
 --
 ALTER TABLE `pesawat`
   ADD PRIMARY KEY (`id_pesawat`),
   ADD UNIQUE KEY `kodePesawat` (`kodePesawat`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `jadwal`
+-- AUTO_INCREMENT untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
-  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_jadwal` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT for table `kota`
+-- AUTO_INCREMENT untuk tabel `kota`
 --
 ALTER TABLE `kota`
-  MODIFY `id_kota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_kota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `pemesanan`
+-- AUTO_INCREMENT untuk tabel `pemesanan`
 --
 ALTER TABLE `pemesanan`
   MODIFY `id_pesan` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `pesawat`
+-- AUTO_INCREMENT untuk tabel `pesawat`
 --
 ALTER TABLE `pesawat`
-  MODIFY `id_pesawat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_pesawat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `jadwal`
+-- Ketidakleluasaan untuk tabel `jadwal`
 --
 ALTER TABLE `jadwal`
   ADD CONSTRAINT `kotaAwalJadwal` FOREIGN KEY (`kodeKotaAwal`) REFERENCES `kota` (`kodeKota`),
@@ -217,7 +225,7 @@ ALTER TABLE `jadwal`
   ADD CONSTRAINT `pesawatJadwal` FOREIGN KEY (`kodePesawat`) REFERENCES `pesawat` (`kodePesawat`);
 
 --
--- Constraints for table `pemesanan`
+-- Ketidakleluasaan untuk tabel `pemesanan`
 --
 ALTER TABLE `pemesanan`
   ADD CONSTRAINT `kota_awal_pesan` FOREIGN KEY (`kota_awal`) REFERENCES `kota` (`id_kota`),
